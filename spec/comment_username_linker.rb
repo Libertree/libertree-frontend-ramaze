ad2a5baf3a47aed55209fcaf609f950e2639e2e4
rekado <rekado@elephly.net>
2012-04-30 07:56:16 +0800

    tests for username linker in comments

---
+++ spec/comment_username_linker.rb
@@ -0,0 +1,93 @@
+require 'redcarpet'
+require 'factory_girl'
+require 'rspec'
+
+require_relative 'factories.rb'
+require_relative '../helper/comment'
+require_relative '../lib/libertree/render'
+require_relative '../lib/libertree/markdown'
+
+describe Ramaze::Helper::Comment do
+  before :each do
+    @s = Object.new.extend(subject)
+
+    @server = Libertree::Model::Server.create( FactoryGirl.attributes_for(:server) )
+    @author = Libertree::Model::Member.create(
+      FactoryGirl.attributes_for(:member, :username => "John Lennon", :server_id => @server.id)
+    )
+    @paul = Libertree::Model::Member.create(
+      FactoryGirl.attributes_for(:member, :username => "Paul McCartney", :server_id => @server.id)
+    )
+    @george = Libertree::Model::Member.create(
+      FactoryGirl.attributes_for(:member, :username => "George Harrison", :server_id => @server.id)
+    )
+    @post = Libertree::Model::Post.create(
+      FactoryGirl.attributes_for(:post, :member_id => @author.id)
+    )
+  end
+
+  describe '#comment_text_rendered_and_participants_linked' do
+    context 'with more than one comment' do
+      it 'should turn @name into a link to last comment by user name' do
+        Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @george.id,
+                                     :post_id => @post.id,
+                                     :text => "Very interesting."))
+        comment = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @paul.id,
+                                     :post_id => @post.id,
+                                     :text => "@George Harrison: indeed. I thought you'd like that."))
+
+        processed = @s.comment_text_rendered_and_participants_linked(comment)
+        processed.should =~ /data-member-id="#{@george.id}"/
+      end
+
+      it 'should replace partial matches that are longer than 2 characters' do
+        Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @george.id,
+                                     :post_id => @post.id,
+                                     :text => "Very interesting."))
+        comment = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @paul.id,
+                                     :post_id => @post.id,
+                                     :text => "@George: indeed. I thought you'd like that."))
+
+        processed = @s.comment_text_rendered_and_participants_linked(comment)
+        processed.should =~ /data-member-id="#{@george.id}"/
+      end
+
+      it 'should replace names irrespective of case' do
+        Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @george.id,
+                                     :post_id => @post.id,
+                                     :text => "Very interesting."))
+        comment = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @paul.id,
+                                     :post_id => @post.id,
+                                     :text => "@george: indeed. I thought you'd like that."))
+        processed = @s.comment_text_rendered_and_participants_linked(comment)
+        processed.should =~ /data-member-id="#{@george.id}"/
+      end
+    end
+
+    context 'with less than two comments' do
+      it 'should not do anything on the first comment' do
+        comment = Libertree::Model::Comment.create(
+          FactoryGirl.attributes_for(:comment,
+                                     :member_id => @paul.id,
+                                     :post_id => @post.id,
+                                     :text => "@George: hope you find this interesting."))
+
+        processed = @s.comment_text_rendered_and_participants_linked(comment)
+        processed.should == comment.text_rendered
+      end
+    end
+
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+require 'libertree/model'
+
+FactoryGirl.define do
+  factory :server, :class => Libertree::Model::Server do
+    sequence(:ip) { |n|
+      m = n / 256 # integer division
+      "192.168.#{m}.#{n}"
+    }
+    public_key OpenSSL::PKey::RSA.new(2048, 65537).public_key.to_pem
+  end
+
+  factory :member, :class => Libertree::Model::Member do
+    sequence(:username) { |n| "member#{n}" }
+    server
+  end
+
+  factory :post, :class => Libertree::Model::Post do
+    sequence(:text) { |n| "Post #{n}" }
+    sequence(:remote_id, 1000)
+    public true
+  end
+
+  factory :comment, :class => Libertree::Model::Comment do
+    sequence(:remote_id, 1000)
+  end
+end
