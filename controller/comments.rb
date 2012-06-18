c72941a8f650467d5d56abbf879102e2d8b894fa
Pistos <github.pistos@purepistos.net>
2012-06-18 10:07:00 -0400

    Update number of comments in excerpt stats after comment received through websocket+AJAX.

--- controller/comments.rb
+++ controller/comments.rb
@@ -59,6 +59,7 @@ module Controller
     def _comment(comment_id)
       @comment = Libertree::Model::Comment[comment_id.to_i]
       @post = @comment.post
+      @render_num_comments = true
     end
 
     def destroy(comment_id)
