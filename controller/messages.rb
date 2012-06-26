0dfa02917bb425ddc67e1863652a1a87c8c92ff5
Pistos <github.pistos@purepistos.net>
2012-06-26 18:35:57 -0400

    DMs: select recipients from among all known members.  Create DM records in local DB (no distribution yet).

--- controller/messages.rb
+++ controller/messages.rb
@@ -18,27 +18,28 @@ module Controller
 
     def index
       @messages = account.messages
+      @contacts = Libertree::Model::Member.all.sort_by(&:name_display)
     end
 
     def create
       redirect_referrer  if ! request.post?
       redirect_referrer  if request['text'].empty?
 
-      # WIP
       message = Libertree::Model::Message.create_with_recipients(
         sender_member_id: account.member.id,
-        text: request['text']
+        text: request['text'],
+        recipient_member_ids: request['recipients']
       )
 
-      Libertree::Model::Job.create(
-        {
-          task: 'request:MESSAGE',
-          params: {
-            'message_id' => message.id,
-            'server_id'  => xx,
-          }.to_json,
-        }
-      )
+      # Libertree::Model::Job.create(
+        # {
+          # task: 'request:MESSAGE',
+          # params: {
+            # 'message_id' => message.id,
+            # 'server_id'  => xx,
+          # }.to_json,
+        # }
+      # )
 
       session[:saved_text]["textarea-message-new"] = nil
 
