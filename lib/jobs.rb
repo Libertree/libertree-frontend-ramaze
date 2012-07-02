d92669e1d252f0155c837ad2c10fa2add1ed9a64
rekado <rekado@elephly.net>
2012-07-02 09:14:26 +0800

    move frontend-specific jobs to frontend repo

---
+++ lib/jobs.rb
@@ -0,0 +1,44 @@
+require 'libertree/model'
+require 'net/http'
+require 'uri'
+
+module Jobs
+  module Frontend
+
+    module FetchAvatar
+      def self.perform(params)
+        member = Libertree::Model::Member[ params['member_id'] ]
+
+        begin
+          uri = URI.parse(params['avatar_url'])
+          if uri.path.empty?
+            log_error "URL contains no path: #{params['avatar_url']}"
+          else
+            Timeout.timeout(10) do
+              Net::HTTP.start(uri.host, uri.port) { |http|
+                resp = http.get(uri.path)
+                ext = File.extname(uri.path)
+                if ! ['.png', '.gif', '.jpg', '.jpeg'].include?(ext.downcase)
+                  log_error "Invalid avatar file type: #{ext}"
+                  # TODO: mark this job as failed
+                else
+                  File.open( "public/images/avatars/#{member.id}#{ext}", 'wb' ) { |file|
+                    file.write(resp.body)
+                  }
+                  member.avatar_path = "/images/avatars/#{member.id}#{ext}"
+                  return true
+                end
+              }
+            end
+          end
+        rescue URI::InvalidURIError, ArgumentError => e
+          # TODO: mark this job as failed, because the URL cannot be parsed
+          log_error "Invalid URI: #{params['avatar_url']}"
+        rescue Timeout::Error
+          # ignore
+        end
+      end
+
+  end
+end
+
