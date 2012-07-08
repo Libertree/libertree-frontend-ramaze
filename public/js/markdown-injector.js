c4edd66fd36053c70dfd69fcd4d0a3da47bc9940
rekado <rekado@elephly.net>
2012-07-08 20:58:18 +0800

    markdown injector: operate on selected text

--- public/js/markdown-injector.js
+++ public/js/markdown-injector.js
@@ -1,7 +1,34 @@
 $(document).ready( function() {
   $('.markdown-injector a').live( 'click', function() {
     var textarea = $(this).closest('.markdown-injector').siblings('textarea');
-    textarea.val( textarea.val() + $(this).data('markdown') );
+    switch ($(this).data('markdown')) {
+      case "title":
+        textarea.surroundSelectedText("\n## ", "");
+        break;
+      case "subtitle":
+        textarea.surroundSelectedText("\n### ", "");
+        break;
+      case "bold":
+        textarea.surroundSelectedText("**", "**");
+        break;
+      case "italic":
+        textarea.surroundSelectedText("*", "*");
+        break;
+      case "url":
+        textarea.surroundSelectedText("[", "](URL)");
+        break;
+      case "image":
+        textarea.surroundSelectedText("![IMAGE TITLE](", ")");
+        break;
+      case "quote":
+        textarea.surroundSelectedText("\n\n> ", "");
+        break;
+      case "list":
+        textarea.surroundSelectedText("\n\n* ", "");
+        break;
+      default:
+        textarea.val( textarea.val() + $(this).data('markdown') );
+    }
     textarea.focus();
     return false;
   } );
