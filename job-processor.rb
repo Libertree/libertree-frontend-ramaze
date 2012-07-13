145516b557d940c53b963df60abe921042883b5b
rekado <rekado@elephly.net>
2012-07-13 22:42:55 +0800

    queue and log_path should be defined in config file

--- job-processor.rb
+++ job-processor.rb
@@ -1,9 +1,11 @@
 require 'libertree/job-processor'
 require_relative 'lib/jobs'
 
-log_path = ARGV[0]
-queue = "test"
+if ARGV[0].nil?
+  $stderr.puts "#{$0} <config.yaml>"
+  exit 1
+end
 
-jobp = Libertree::JobProcessor.new( log_path, queue )
+jobp = Libertree::JobProcessor.new( ARGV[0] )
 jobp.extend Jobs
 jobp.run
