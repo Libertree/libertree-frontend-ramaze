221721b00988fd19e1314b75e15797d6dcc52d4f
Pistos <github.pistos@purepistos.net>
2012-07-15 12:56:34 -0400

    Write out pids/websocket-server.pid .

--- websocket-server.rb
+++ websocket-server.rb
@@ -6,6 +6,17 @@ require 'libertree/model'
 $conf = Syck.load( File.read("#{ File.dirname( __FILE__ ) }/config/application.yaml") )
 $sessions = Hash.new
 
+pid_dir = File.join( File.dirname(__FILE__), 'pids' )
+if ! Dir.exists?(pid_dir)
+  Dir.mkdir pid_dir
+end
+pid_file = File.join(pid_dir, 'websocket-server.pid')
+File.open(pid_file, 'w') do |f|
+  f.print Process.pid
+end
+
+puts "Starting websocket server (pid #{Process.pid})..."
+
 def onmessage(ws, data)
   sid = data['sid']
   session_account = Libertree::Model::SessionAccount[sid: sid]
