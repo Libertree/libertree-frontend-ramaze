94590612048ee1c3903d93d1574c90560d1effcc
Pistos <github.pistos@purepistos.net>
2012-07-26 19:56:17 -0400

    Refuse to run specs in a non-test LIBERTREE_ENV.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+if ENV['LIBERTREE_ENV'] != 'test'
+  $stderr.puts "Are you sure you want to run tests in a non-test LIBERTREE_ENV?"
+  $stderr.puts "Comment out this check in spec_helper.rb if you know what you're doing."
+  exit 1
+end
+
 require_relative '../app'
 require_relative 'factories'
 require 'capybara/rspec'
