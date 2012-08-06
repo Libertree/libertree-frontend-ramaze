82333f7b1dd4fc27bd0735ff8dfd24e8c1923b19
Pistos <github.pistos@purepistos.net>
2012-08-05 21:18:50 -0400

    Set post visibility on creation and update.

--- controller/posts.rb
+++ controller/posts.rb
@@ -57,9 +57,9 @@ module Controller
       end
 
       post = Libertree::Model::Post.create(
-        'member_id' => account.member.id,
-        'public'    => true,
-        'text'      => text
+        'member_id'  => account.member.id,
+        'visibility' => request['visibility'].to_s,
+        'text'       => text
       )
       session[:saved_text]['textarea-post-new'] = nil
 
@@ -145,7 +145,7 @@ module Controller
         text.encode!('UTF-16', 'UTF-8', :invalid => :replace, :replace => '?')
         text.encode!('UTF-8', 'UTF-16')
 
-        post.revise text
+        post.revise text, request['visibility'].to_s
         session[:saved_text]['textarea-post-edit'] = nil
       end
 
