85ce177f83f9c24c706763b88337b1f816b74487
rekado <rekado@elephly.net>
2012-08-06 18:29:59 +0800

    gettextify invitations controller

--- controller/invitations.rb
+++ controller/invitations.rb
@@ -8,7 +8,7 @@ module Controller
 
     def create
       if account.new_invitation.nil?
-        flash[:error] = "Failed to create invitation.  You may only have up to 5 unaccepted invitations at once."
+        flash[:error] = _('Failed to create invitation.  You may only have up to 5 unaccepted invitations at once.')
       end
 
       redirect_referrer
