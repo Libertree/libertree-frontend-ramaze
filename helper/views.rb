0e8d81a5b9a94b79b6a3df2276a2d809d31f7d5b
rekado <rekado@elephly.net>
2012-08-08 23:11:25 +0800

    load css files from themes directory

--- helper/views.rb
+++ helper/views.rb
@@ -10,7 +10,9 @@ module Ramaze
         "<script src=\"/js/#{file}.js?t=#{File.mtime("public/js/#{file}.js").to_i}\" type=\"text/javascript\"></script>"
       end
       def css_nocache(file, media="screen")
-        "<link href=\"/css/#{file}.css?t=#{File.mtime("public/css/#{file}.css").to_i}\" media=\"#{media}\" rel=\"stylesheet\" type=\"text/css\" />"
+        #TODO: use theme setting
+        theme = 'default'
+        "<link href=\"/themes/#{theme}/css/#{file}.css?t=#{File.mtime("public/themes/#{theme}/css/#{file}.css").to_i}\" media=\"#{media}\" rel=\"stylesheet\" type=\"text/css\" />"
       end
     end
   end
