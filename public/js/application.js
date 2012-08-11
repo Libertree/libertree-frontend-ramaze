17c7081fb0294c066cdf950f1fbdf9ec9ca9fcc9
rekado <rekado@elephly.net>
2012-08-12 07:00:11 +0800

    remove textarea auto-resize feature

--- public/js/application.js
+++ public/js/application.js
@@ -158,7 +158,6 @@ $(document).ready( function() {
 
   setInterval( updateAges, 60 * 1000 );
   timerSaveTextAreas = setInterval( saveTextAreaText, 15 * 1000 );
-  $('textarea').not('.textarea-chat').expandable();
 
   if( layout == 'narrow' ) {
     $('*').mouseover();
