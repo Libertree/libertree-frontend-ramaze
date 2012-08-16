86bfc3ed256851a0b128ca74c7bd71830de11bcf
rekado <rekado@elephly.net>
2012-08-16 17:00:42 +0800

    remove duplicated cookie reading code

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,17 +1,6 @@
 var host = null;
 var ws = null;
 
-function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
 $(document).ready( function() {
   if( 'MozWebSocket' in window ) {
     ws = new MozWebSocket("ws://" + host + ":8080");
@@ -22,7 +11,7 @@ $(document).ready( function() {
   }
 
   ws.onopen = function(e) {
-    this.send('{ "sid": "' + getCookie('innate.sid') + '" }');
+    this.send('{ "sid": "' + $.cookie('innate.sid') + '" }');
   }
 
   ws.onmessage = function(e) {
