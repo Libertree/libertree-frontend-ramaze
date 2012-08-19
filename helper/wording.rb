40758451bb6c30ecd48aa2fb8eea3654b90bf9c7
rekado <rekado@elephly.net>
2012-08-20 07:42:36 +0800

    remove obsolete plural_s function

--- helper/wording.rb
+++ helper/wording.rb
@@ -1,11 +1,6 @@
 module Ramaze
   module Helper
     module Wording
-      # TODO: to be removed
-      def plural_s(n)
-        n != 1  ? 's' : ''
-      end
-
       def anded_list(array)
         if array.length < 2
           array[0].to_s
