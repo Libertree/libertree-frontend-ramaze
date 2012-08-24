ecff4a2feb3811253cf8a0cfa91fa3ab8ed84091
rekado <rekado@elephly.net>
2012-08-24 17:06:28 +0800

    search results with longer snippets

--- controller/base.rb
+++ controller/base.rb
@@ -1,6 +1,6 @@
 module Controller
   class Base < Ramaze::Controller
-    helper :user, :xhtml, :age, :comment, :member, :wording, :views, :post
+    helper :user, :xhtml, :age, :comment, :member, :wording, :views, :post, :search
     trait :user_model => ::Libertree::Model::Account
 
     layout do |path|
