3b363d98985b831ed53a7fddaaa88bf6b3fba3d6
rekado <rekado@elephly.net>
2012-08-31 17:12:05 +0800

    interface to initiate account deletion

--- controller/accounts.rb
+++ controller/accounts.rb
@@ -92,6 +92,21 @@ module Controller
       account.data_hash
     end
 
+    def delete
+      return  if ! request.post?
+
+      if account.username != request['username'].to_s
+        flash[:error] = s_('account-delete|The username you provided does not match your username. Please input your own username to confirm account deletion.')
+      else
+        account_logout
+        account.delete_cascade
+        session = nil
+        flash[:notice] = s_('account-delete|Your account has been deleted.')
+        # TODO: redirect to goodbye page instead
+        redirect Main.r(:login)
+      end
+    end
+
     def heartbeat
       account.time_heartbeat = Time.now
     end
