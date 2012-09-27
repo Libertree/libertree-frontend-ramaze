0176fe67ba37d4b0b5c5905cc7b21de1a2cfc9e9
rekado <rekado@elephly.net>
2012-09-27 18:50:01 +0800

    ignore hashtag inside of link texts

--- lib/libertree/render.rb
+++ lib/libertree/render.rb
@@ -46,7 +46,7 @@ module Libertree
 
     # hashtaggify everything that is not inside of code or pre tags
     html.traverse do |node|
-      if node.ancestors("code").empty? && node.ancestors("pre") && node.text?
+      if node.text? && ["code", "pre", "a"].all? {|tag| node.ancestors(tag).empty? }
         hashtag = Libertree::hashtaggify(node.text)
         if ! hashtag.eql? node.text
           node.replace hashtag
