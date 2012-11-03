4cc4964c70ee788ab953317f540667d0df940c4b
Pistos <github.pistos@purepistos.net>
2012-11-03 00:49:13 -0400

    Fix maple-specific bug which made UTF-8 characters get messed up when in the same paragraph as hashtags.

--- lib/libertree/render.rb
+++ lib/libertree/render.rb
@@ -54,7 +54,7 @@ module Libertree
       if node.text? && ["code", "pre", "a"].all? {|tag| node.ancestors(tag).empty? }
         hashtag = Libertree::hashtaggify(node.text)
         if ! hashtag.eql? node.text
-          node.replace hashtag
+          node.replace( Nokogiri::HTML.fragment(hashtag) )
         end
       end
     end
