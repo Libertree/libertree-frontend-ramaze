d97d2ddb62d280699c4eba75a7fe7f554d1094df
rekado <rekado@elephly.net>
2012-11-09 22:27:31 +0800

    determine update function in closure

--- public/js/likes.js
+++ public/js/likes.js
@@ -1,30 +1,30 @@
 Libertree.mkLike = function(type, entityPath) {
+  // TODO: merge the two update functions
+  var update = (type === 'comment') ?
+    function( entity, response ) {
+      var num_likes = entity.find('.num-likes');
+      num_likes.text( response['num_likes'] );
+      num_likes.attr('title', response['liked_by']);
+      num_likes.show();
+    }
+  :
+    function( entity, response ) {
+      /* TODO: Clean up this selector */
+      var num_likes = entity.find('.post-tools .num-likes, .post-stats .num-likes');
+      num_likes.find('.value').text( response['num_likes'] );
+      num_likes.attr('title', response['liked_by']);
+      num_likes.show();
+
+      // mark post read
+      if( entity.find('.mark-unread.hidden').length ) {
+        entity.find('.mark-read').addClass('hidden');
+        entity.find('.mark-unread').removeClass('hidden');
+      }
+    }
+  ;
+
   return function(event) {
     event.preventDefault();
-
-    // TODO: merge the two update functions
-    var update = (type === 'comment') ?
-      function( entity, response ) {
-        var num_likes = entity.find('.num-likes');
-        num_likes.text( response['num_likes'] );
-        num_likes.attr('title', response['liked_by']);
-        num_likes.show();
-      }
-    :
-      function( entity, response ) {
-        /* TODO: Clean up this selector */
-        var num_likes = entity.find('.post-tools .num-likes, .post-stats .num-likes');
-        num_likes.find('.value').text( response['num_likes'] );
-        num_likes.attr('title', response['liked_by']);
-        num_likes.show();
-
-        // mark post read
-        if( entity.find('.mark-unread.hidden').length ) {
-          entity.find('.mark-read').addClass('hidden');
-          entity.find('.mark-unread').removeClass('hidden');
-        }
-      }
-    ;
 
     var link = $(this);
     var entity = link.closest(entityPath);
@@ -45,32 +45,32 @@ Libertree.mkLike = function(type, entityPath) {
 
 
 Libertree.mkUnlike = function(type, entityPath) {
+  // TODO: merge these update functions
+  var update = (type === 'comment') ?
+    function( entity, response ) {
+      var num_likes = entity.find('.num-likes');
+      num_likes.text( response['num_likes'] );
+      // FIXME: gettextify
+      if( response['num_likes'] === '0 likes' ) {
+        num_likes.hide();
+      } else {
+        num_likes.attr('title', response['liked_by']);
+      }
+    }
+  :
+    function( entity, response ) {
+      /* TODO: Clean up this selector */
+      var num_likes = entity.find('.post-tools .num-likes, .post-stats .num-likes');
+      num_likes.find('.value').text( response['num_likes'] );
+      num_likes.attr('title', response['liked_by']);
+      if( response['num_likes'] === 0 ) {
+        num_likes.hide();
+      }
+    }
+  ;
+
   return function(event) {
     event.preventDefault();
-
-    // TODO: merge these update functions
-    var update = (type === 'comment') ?
-      function( entity, response ) {
-        var num_likes = entity.find('.num-likes');
-        num_likes.text( response['num_likes'] );
-        // FIXME: gettextify
-        if( response['num_likes'] === '0 likes' ) {
-          num_likes.hide();
-        } else {
-          num_likes.attr('title', response['liked_by']);
-        }
-      }
-    :
-      function( entity, response ) {
-        /* TODO: Clean up this selector */
-        var num_likes = entity.find('.post-tools .num-likes, .post-stats .num-likes');
-        num_likes.find('.value').text( response['num_likes'] );
-        num_likes.attr('title', response['liked_by']);
-        if( response['num_likes'] === 0 ) {
-          num_likes.hide();
-        }
-      }
-    ;
 
     var link = $(this);
     var entity = link.closest(entityPath);
