3dda11d6d7f883f4621d64c5fa5ea1b80267c5cf
rekado <rekado@elephly.net>
2012-12-15 22:42:39 +0800

    symlink view instead of using `render_file`
    Posts containing strings like "\x" are not properly escaped when using
    render_file.

--- controller/profiles_local.rb
+++ controller/profiles_local.rb
@@ -23,11 +23,9 @@ module Controller
       if account.nil?
         redirect_referrer
       end
-      @posts = account.member.posts
-
-      render_file("#{Ramaze.options.views[0]}/profiles/show.xhtml",
-                  { :profile => account.member.profile,
-                    :member => account.member })
+      @member = account.member
+      @profile = @member.profile
+      @posts = @member.posts
     end
   end
 end
