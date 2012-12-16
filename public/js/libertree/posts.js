c087a0091b64562fa963ffb0e52c3c137a95e5a7
rekado <rekado@elephly.net>
2012-12-16 10:09:01 +0800

    move checkForHashtags into Libertree.Posts

--- public/js/libertree/posts.js
+++ public/js/libertree/posts.js
@@ -8,5 +8,24 @@ Libertree.Posts = {
         post.find('.mark-unread').removeClass('hidden');
       }
     );
+  },
+
+  hashtagCheckTimer: undefined,
+  checkForHashtags: function() {
+    if (Libertree.Posts.hashtagCheckTimer != undefined) {
+      clearTimeout(Libertree.Posts.hashtagCheckTimer);
+    }
+    Libertree.Posts.hashtagCheckTimer = setTimeout(
+      Libertree.Posts.checkForHashtagsCallback,
+      500
+    );
+  },
+
+  checkForHashtagsCallback: function() {
+    if( $('#post-new textarea').val().match(/#[a-zA-Z0-9_-]/) ) {
+      $('#hashtags-input').hide();
+    } else {
+      $('#hashtags-input').show();
+    }
   }
 };
