7bea9278a99c06f69648a8d3df8885b9ae554efa
rekado <rekado@elephly.net>
2013-01-05 18:49:14 +0800

    remove dead code

--- public/js/post-new.js
+++ public/js/post-new.js
@@ -1,17 +1,8 @@
 $(document).ready( function() {
-  $('a.post-new, #menu-new-post').click( function() {
-    window.location = '/posts/new';
-    return false;
-  } );
-
   $('#post-new textarea').live( 'keyup',  Libertree.Posts.checkForHashtags );
   $('#post-new textarea').live( 'change', Libertree.Posts.checkForHashtags );
 
   $('#post-new input[type="submit"]').live( 'click', function() {
     Libertree.UI.TextAreaBackup.disable();
   } );
-
-  if( document.URL.match(/#post-new/) ) {
-    $('a.post-new, #menu-new-post').click();
-  }
 } );
