ee167e87dbb50c07acbc12fd8fc5c113673b431b
rekado <rekado@elephly.net>
2013-03-11 17:53:38 +0800

    gettextify comment delete message

--- public/js/comments.js
+++ public/js/comments.js
@@ -29,9 +29,8 @@ $(document).ready( function() {
 
   $('.comment .delete').live( 'click', function(event) {
     event.preventDefault();
-    // TODO: gettextify
-    if( confirm('Delete this comment?') ) {
-      var comment = $(this).closest('.comment');
+    var comment = $(this).closest('.comment');
+    if( confirm(comment.find('.delete').data('msg')) ) {
       $.get( '/comments/destroy/' + comment.data('comment-id') );
       comment.fadeOut( function() { comment.remove } );
     }
