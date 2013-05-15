e9c85a66aebba01bbc9bf23584de3d3a9be4d140
rekado <rekado@elephly.net>
2013-05-15 21:03:11 +0800

    appease JSLint

--- public/js/libertree/post_loader.js
+++ public/js/libertree/post_loader.js
@@ -1,58 +1,63 @@
-Libertree.PostLoader = (function() {
-  var mkLoader = function( type ) {
-    var endpoint,
-        loading = false;
-        self = this;
+/*jslint indent: 2, todo: true */
+/*global $ */
 
-    switch( type ) {
-      case 'river':
-        endpoint = '/posts/_excerpts';
-        break;
-      case 'profile':
-        endpoint = '/profiles/_more';
-        break;
-      case 'tags':
-        endpoint = '/tags/_more';
-        break;
-      case 'pool':
-        endpoint = '/pools/_more';
-        break;
-      default:
-        // not supported
-        return function(){};
+Libertree.PostLoader = (function () {
+  'use strict';
+
+  var mkLoader = function (type) {
+    var endpoint,
+      loading = false,
+      self = this;
+
+    switch (type) {
+    case 'river':
+      endpoint = '/posts/_excerpts';
+      break;
+    case 'profile':
+      endpoint = '/profiles/_more';
+      break;
+    case 'tags':
+      endpoint = '/tags/_more';
+      break;
+    case 'pool':
+      endpoint = '/pools/_more';
+      break;
+    default:
+      // not supported
+      return function () {};
     }
 
-    return function( value, older_or_newer, time, onSuccess ) {
+    return function (value, older_or_newer, time, onSuccess) {
       if (self.loading === true || value === undefined) { return; }
 
       if (older_or_newer === undefined) {
         older_or_newer = 'older';
       }
       if (time === undefined) {
-        time = $('.post-excerpt:last').data('t')
+        time = $('.post-excerpt:last').data('t');
       }
 
       self.loading = true;
       Libertree.UI.addSpinner('#post-excerpts div.spinner', 'append');
 
-      $.ajax( {
+      $.ajax({
         type: 'GET',
         url: endpoint + '/' + value + '/' + older_or_newer + '/' + time,
-        success: function(html) {
-          var DOMNodes = $( $.trim(html) ),
-              excerpts = Libertree.UI.animatableNodesOnly(DOMNodes);
+        success: function (html) {
+          var DOMNodes = $($.trim(html)),
+            excerpts = Libertree.UI.animatableNodesOnly(DOMNodes);
 
           excerpts.css('display', 'none');
 
-          if( older_or_newer === 'newer' ) {
+          if (older_or_newer === 'newer') {
             $('#post-excerpts').prepend(excerpts);
           } else {
             $('#post-excerpts').append(excerpts);
           }
           Libertree.UI.makeTextAreasExpandable();
-          excerpts.slideDown( function() {
+          excerpts.slideDown(function () {
             self.loading = false;
-          } );
+          });
 
           // move the spinner container to the bottom of the stream
           $('#post-excerpts div.spinner').appendTo($('#post-excerpts'));
@@ -60,18 +65,18 @@ Libertree.PostLoader = (function() {
 
           //TODO: only process new excerpts!
           Libertree.UI.showShowMores();
-          if(onSuccess) {
+          if (onSuccess) {
             onSuccess();
           }
         }
-      } );
+      });
     };
   };
 
   return {
-    loadFromRiver:   mkLoader( 'river'   ),
-    loadFromTags:    mkLoader( 'tags'    ),
-    loadFromProfile: mkLoader( 'profile' ),
-    loadFromPool:    mkLoader( 'pool'    ),
+    loadFromRiver:   mkLoader('river'),
+    loadFromTags:    mkLoader('tags'),
+    loadFromProfile: mkLoader('profile'),
+    loadFromPool:    mkLoader('pool')
   };
 }());
