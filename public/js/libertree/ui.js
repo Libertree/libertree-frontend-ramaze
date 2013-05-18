895521852291bc187e873f1b3c8f9cc82bd96fb9
rekado <rekado@elephly.net>
2013-05-18 21:04:49 +0800

    move markdown injector function into Libertree.UI

--- public/js/libertree/ui.js
+++ public/js/libertree/ui.js
@@ -35,6 +35,48 @@ Libertree.UI = (function () {
       }
     },
 
+    markdownInjector: function () {
+      var $this = $(this),
+        textarea = $this.closest('.markdown-injector').siblings('textarea');
+
+      switch ($this.data('markdown')) {
+      case "title":
+        textarea.surroundSelectedText("\n## ", "");
+        break;
+      case "subtitle":
+        textarea.surroundSelectedText("\n### ", "");
+        break;
+      case "bold":
+        textarea.surroundSelectedText("**", "**");
+        break;
+      case "italic":
+        textarea.surroundSelectedText("*", "*");
+        break;
+      case "strike":
+        textarea.surroundSelectedText("~~", "~~");
+        break;
+      case "url":
+        textarea.surroundSelectedText("[", "](URL)");
+        break;
+      case "image":
+        textarea.surroundSelectedText("![IMAGE TITLE](", ")");
+        break;
+      case "image-link":
+        textarea.surroundSelectedText("[![image/photo](", ")](URL)");
+        break;
+      case "quote":
+        textarea.surroundSelectedText("\n\n> ", "");
+        break;
+      case "list":
+        textarea.surroundSelectedText("\n\n* ", "");
+        break;
+      default:
+        textarea.val( textarea.val() + $this.data('markdown') );
+      }
+      textarea.focus();
+      return false;
+    },
+
     hideWindows: function() {
       $('#chat-window.resizable').resizable('destroy');
       $('#chat-window').removeClass('resizable');
