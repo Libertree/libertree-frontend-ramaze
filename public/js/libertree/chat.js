5be63ef2302453cfe96ea0ecad1f692661ed58b4
rekado <rekado@elephly.net>
2013-05-18 21:56:33 +0800

    move chat initialisation to Libertree.Chat.init

--- public/js/libertree/chat.js
+++ public/js/libertree/chat.js
@@ -132,6 +132,142 @@ Libertree.Chat = (function () {
 
     heartbeat: function() {
       $.get('/accounts/heartbeat');
+    },
+
+    init: function () {
+      $('#menu-chat').click( function() {
+        if( $('#chat-window').is(':visible') ) {
+          Libertree.UI.hideWindows();
+          return false;
+        }
+
+        Libertree.UI.hideWindows();
+        $('#chat-window').empty();
+        Libertree.UI.addSpinner('#chat-window', 'append');
+        $('#chat-window')
+          .show()
+          .load(
+            '/chat/_index',
+            function(html) {
+              Libertree.Session.ensureAlive(html);
+              Libertree.UI.removeSpinner('#chat-window');
+              $('#chat-window').hide();
+              var o = $( $.trim(html) );
+              Libertree.Chat.markConversationSeen( o.find('.log.active').data('member-id') );
+
+              $('#chat-window')
+                .resizable( {
+                  minHeight: 170,
+                  resize: function(event, ui) {
+                    Libertree.Chat.syncUIDimensions();
+                  },
+                  stop: function(event, ui) {
+                    Libertree.Chat.rememberDimensions();
+                  }
+                } )
+              ;
+              $('#chat-window').addClass('resizable');
+
+              $('select#chat-new-partner').chosen().change( function() {
+                var memberId = $('select#chat-new-partner').val();
+                Libertree.Chat.fetchConversationWith(memberId, true);
+                $('select#chat-new-partner').val('0');
+                $('select#chat-new-partner').trigger("liszt:updated");
+                return false;
+              } );
+
+              Libertree.Chat.syncUIDimensions();
+              $('#chat-window').show();
+              $('#chat-window .log .messages').scrollTop(999999);
+              $('#chat-window .log.active .textarea-chat').focus();
+            }
+          )
+        ;
+        Libertree.Chat.rememberDimensions();
+
+        return false;
+      } );
+
+      $(document).on('click', '#chat-window .tab', function() {
+        var memberId = $(this).data('member-id');
+        Libertree.Chat.activateConversation(memberId);
+        Libertree.Chat.markConversationSeen(memberId);
+      } );
+
+      $(document).on('keydown', '#chat-window .textarea-chat', function(event) {
+        if( event.keyCode != 13 ) {
+          return;
+        }
+
+        var textarea = $(this);
+
+        textarea.prop('disabled', true);
+        Libertree.UI.TextAreaBackup.disable();
+        var memberId = textarea.closest('.log').data('member-id');
+
+        $.post(
+          '/chat/create',
+          {
+            to_member_id: memberId,
+            text: textarea.val()
+          },
+          function(response) {
+            var h = $.parseJSON(response);
+            if( h.success ) {
+              textarea.val('');
+            } else {
+              alert('Failed to send chat message.');
+            }
+            textarea.prop('disabled', false);
+          }
+        );
+      } );
+
+      $(document).on('click', '#chat-window .tab .close', function() {
+        var tab = $(this).closest('.tab');
+        var memberId = tab.data('member-id');
+        var tabToActivate = tab.next();
+        if( tabToActivate.length === 0 ) {
+          tabToActivate = tab.prev();
+        }
+        $('#chat-window .tab[data-member-id="'+memberId+'"]').remove();
+        $('#chat-window .log[data-member-id="'+memberId+'"]').remove();
+        $.get('/chat/closed/'+memberId);
+        if( $('#chat-window .tab.active').length === 0 && tabToActivate.length ) {
+          Libertree.Chat.activateConversation( tabToActivate.data('member-id') );
+        }
+        return false;
+      } );
+
+      $('#chat-window').draggable( {
+        handle: '.header',
+        stop: function(event, ui) {
+          Libertree.Chat.rememberDimensions();
+        }
+      } );
+
+      $(document).on('click', '#online-contacts .avatar', function() {
+        Libertree.Chat.fetchConversationWith( $(this).data('member-id'), true);
+        return false;
+      } );
+
+      /* ------------------------------------------------------ */
+
+      $.cookie('chat-width', $.cookie('chat-width') || 400);
+      $.cookie('chat-height', $.cookie('chat-height') || 400);
+      $('#chat-window').css( {
+        top: $.cookie('chat-top'),
+        left: $.cookie('chat-left'),
+        width: $.cookie('chat-width'),
+        height: $.cookie('chat-height')
+      } );
+      if( $.cookie('chat-open') === 'true' ) {
+        $('#menu-chat').click();
+      }
+
+      setInterval( Libertree.Chat.heartbeat, 3 * 60 * 1000 );
     }
   };
 }());
+
+$(document).ready( Libertree.Chat.init );
