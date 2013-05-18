3785ae822a331c4e08e0778247c8879f4d9b071a
rekado <rekado@elephly.net>
2013-05-18 18:13:23 +0800

    make most functions private in Libertree.Posts

--- public/js/libertree/posts.js
+++ public/js/libertree/posts.js
@@ -5,40 +5,23 @@ Libertree.Posts = (function () {
   "use strict";
 
   var setSubscription = function(type) {
-    var endpoint = '/posts/_' + type + '/',
-        classes = ['.subscribe', '.unsubscribe'],
-        toggle = (type === 'subscribe') ? classes : classes.reverse();
+      var endpoint = '/posts/_' + type + '/',
+          classes = ['.subscribe', '.unsubscribe'],
+          toggle = (type === 'subscribe') ? classes : classes.reverse();
 
-    return function (post) {
-      var icon = post.find(toggle[0] + ' img');
-      Libertree.UI.enableIconSpinner(icon);
-      $.get( endpoint + post.data('post-id'),
-             function () {
-               Libertree.UI.disableIconSpinner(icon);
-               post.find(toggle[0]).addClass('hidden');
-               post.find(toggle[1]).removeClass('hidden');
-             });
-    };
-  };
-
-  return {
-    subscribe:   setSubscription('subscribe'),
-    unsubscribe: setSubscription('unsubscribe'),
-    like:        Libertree.mkLike('post'),
-    unlike:      Libertree.mkUnlike('post'),
-    markRead: function(post_id) {
-      $.get(
-        '/posts/_read/' + post_id,
-        function() {
-          var post = $('*[data-post-id="'+post_id+'"]');
-          Libertree.UI.disableIconSpinner(post.find('.mark-read img'));
-          post.find('.mark-read').addClass('hidden');
-          post.find('.mark-unread').removeClass('hidden');
-        }
-      );
+      return function (post) {
+        var icon = post.find(toggle[0] + ' img');
+        Libertree.UI.enableIconSpinner(icon);
+        $.get( endpoint + post.data('post-id'),
+               function () {
+                 Libertree.UI.disableIconSpinner(icon);
+                 post.find(toggle[0]).addClass('hidden');
+                 post.find(toggle[1]).removeClass('hidden');
+               });
+      };
     },
 
-    hide: function(post_id, onSuccess) {
+    hide = function(post_id, onSuccess) {
       $.get(
         '/posts/hidden/create/' + post_id + '.json',
         function(response) {
@@ -50,21 +33,41 @@ Libertree.Posts = (function () {
       );
     },
 
+    markRead = function(post_id) {
+      $.get(
+        '/posts/_read/' + post_id,
+        function() {
+          var post = $('*[data-post-id="'+post_id+'"]');
+          Libertree.UI.disableIconSpinner(post.find('.mark-read img'));
+          post.find('.mark-read').addClass('hidden');
+          post.find('.mark-unread').removeClass('hidden');
+        }
+      );
+    },
+
+    subscribe   = setSubscription('subscribe'),
+    unsubscribe = setSubscription('unsubscribe'),
+    like        = Libertree.mkLike('post'),
+    unlike      = Libertree.mkUnlike('post');
+
+  return {
+    markRead: markRead,
+
     init: function() {
       $(document).ready( function() {
 
         $(document).on('click', '.post-tools a.like', function(event) {
-          Libertree.Posts.like( $(this), event, 'div.post, .post-excerpt' );
+          like( $(this), event, 'div.post, .post-excerpt' );
         } );
 
         $(document).on('click', '.post-tools a.unlike', function(event) {
-          Libertree.Posts.unlike( $(this), event, 'div.post, .post-excerpt' );
+          unlike( $(this), event, 'div.post, .post-excerpt' );
         } );
 
         $(document).on('click', '.mark-read', function() {
           var post = $(this).closest('div.post, div.post-excerpt');
           Libertree.UI.enableIconSpinner(post.find('.mark-read img'));
-          Libertree.Posts.markRead( post.data('post-id') );
+          markRead( post.data('post-id') );
           return false;
         } );
 
@@ -112,7 +115,7 @@ Libertree.Posts = (function () {
         $(document).on('click', '.excerpts-view .confirm-hide', function(event) {
           event.preventDefault();
           var post = $(this).closest('div.post-excerpt');
-          Libertree.Posts.hide(
+          hide(
             post.data('post-id'),
             function() {
               post.
@@ -128,7 +131,7 @@ Libertree.Posts = (function () {
         $(document).on('click', '.single-post-view .confirm-hide', function(event) {
           event.preventDefault();
           var post = $(this).closest('div.post');
-          Libertree.Posts.hide(
+          hide(
             post.data('post-id'),
             function() {
               window.location = '/home';
@@ -146,12 +149,12 @@ Libertree.Posts = (function () {
 
         $(document).on('click', '.post-tools .subscribe', function() {
           var post = $(this).closest('div.post, div.post-excerpt');
-          Libertree.Posts.subscribe( post );
+          subscribe( post );
           return false;
         } );
         $(document).on('click', '.post-tools .unsubscribe', function() {
           var post = $(this).closest('div.post, div.post-excerpt');
-          Libertree.Posts.unsubscribe( post );
+          unsubscribe( post );
           return false;
         } );
       } );
