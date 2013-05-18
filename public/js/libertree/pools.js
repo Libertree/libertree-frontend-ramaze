740904bfeee5c085ea59a24a1d2f24f847a7b109
rekado <rekado@elephly.net>
2013-05-18 16:56:04 +0800

    explicitly export public functions
    As the whole module is now wrapped in an immediate function, the
    majority of the changes are due to adjustments to indentation.

--- public/js/libertree/pools.js
+++ public/js/libertree/pools.js
@@ -1,42 +1,48 @@
-Libertree.Pools = {
-  createPoolAndAddPost: function(post) {
-    var postId = post.data('post-id'),
-      textField = post.find('.pools .chzn-search input'),
-      poolName = textField.val();
+/*jslint white: true, indent: 2, todo: true */
+/*global $, alert, Libertree */
 
-    textField.prop('disabled', true);
+Libertree.Pools = (function () {
+  "use strict";
 
-    $.get(
-      '/pools/create_pool_and_add_post/'+poolName+'/'+postId,
-      function(response) {
-        var h = $.parseJSON(response);
-        if(h.success) {
-          $('.pools.window').hide();
-          post.find('a.collect').addClass('hidden');
-          post.find('a.collected').removeClass('hidden');
-        } else {
-          alert(h.msg);
+  return {
+    createPoolAndAddPost: function(post) {
+      var postId = post.data('post-id'),
+        textField = post.find('.pools .chzn-search input'),
+        poolName = textField.val();
+
+      textField.prop('disabled', true);
+
+      $.get(
+        '/pools/create_pool_and_add_post/'+poolName+'/'+postId,
+        function(response) {
+          var h = $.parseJSON(response);
+          if(h.success) {
+            $('.pools.window').hide();
+            post.find('a.collect').addClass('hidden');
+            post.find('a.collected').removeClass('hidden');
+          } else {
+            alert(h.msg);
+          }
+          textField.prop('disabled', false);
         }
-        textField.prop('disabled', false);
-      }
-    );
-  },
+      );
+    },
 
-  addPost: function(poolId, postId, post, x, y) {
-    $.get(
-      '/pools/add_post/' + poolId + '/' + postId,
-      function(response) {
-        var h = $.parseJSON(response);
-        $('div.pools').remove();
-        if(h.success) {
-          post.find('a.collect').addClass('hidden');
-          post.find('a.collected').removeClass('hidden');
-          Libertree.UI.fadingAlert(h.msg, x, y);
-        } else {
-          alert(h.msg);
+    addPost: function(poolId, postId, post, x, y) {
+      $.get(
+        '/pools/add_post/' + poolId + '/' + postId,
+        function(response) {
+          var h = $.parseJSON(response);
+          $('div.pools').remove();
+          if(h.success) {
+            post.find('a.collect').addClass('hidden');
+            post.find('a.collected').removeClass('hidden');
+            Libertree.UI.fadingAlert(h.msg, x, y);
+          } else {
+            alert(h.msg);
+          }
         }
-      }
-    );
-  }
-
-};
+      );
+    }
+  };
+}());
