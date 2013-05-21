53961c4ae21bdcd69cd0b7eb0997d0d434cfe89d
rekado <rekado@elephly.net>
2013-05-21 18:01:29 +0800

    don't set window.location, use href

--- public/js/controller/rivers.js
+++ public/js/controller/rivers.js
@@ -18,10 +18,8 @@ $(document).ready( function() {
   } );
 
   $(document).on('click', '.river-tools .delete', function(event) {
-    event.preventDefault();
-    if( confirm($(this).data('msg')) ) {
-      var river = $(this).closest('div[data-river-id]');
-      window.location = '/rivers/destroy/' + river.data('river-id');
+    if( ! confirm($(this).data('msg')) ) {
+      event.preventDefault();
     }
   } );
 } );
