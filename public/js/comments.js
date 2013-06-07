Libertree/libertree-frontend-ramaze

commit 9adfb3e5939435f839409dd58e2d3e3119c84165

use window.location.hash instead of regexp

public/js/comments.js
@@ -151,9 +151,7 @@ $(document).ready( function() {
 
   /* ---------------------------------------------------- */
 
-  // TODO: replace with window.location.hash
-  var match = document.URL.match(/#comment-([0-9]+)/);
-  if( match ) {
+  if( window.location.hash.length > 0 ) {
     window.location = window.location;  /* Hack for Firefox */
     Libertree.Comments.loadMore( $('a.load-comments'), true );
   }

commit b88289877771df4b7ba49010a41e4c607c31c4f7

check location hash for #comment-
fixes regression introduced by 9adfb3e5939435f8

public/js/comments.js
@@ -151,7 +151,7 @@ $(document).ready( function() {
 
   /* ---------------------------------------------------- */
 
-  if( window.location.hash.length > 0 ) {
+  if( window.location.hash.startsWith("#comment-") ) {
     Libertree.Comments.loadMore( $('a.load-comments'), true );
   }
 