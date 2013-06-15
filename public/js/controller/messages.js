e6d4b8b5b296b63fa783c2aa3d7a1342929a9855
rekado <rekado@elephly.net>
2013-06-15 22:52:44 +0800

    use participants JSON to implement reply feature with select2

--- public/js/controller/messages.js
+++ public/js/controller/messages.js
@@ -4,22 +4,14 @@ $(document).ready( function() {
     return false;
   } );
 
-  $('a.reply').click( function() {
-    $('form#new-message #recipients option').prop('selected', false);
-    $('form#new-message #recipients option[value="'+$('.sender').data('member-id')+'"]').prop('selected', true);
-    $('select#recipients').trigger("liszt:updated");
-    return false;
+  $('a.reply').click( function (event) {
+    event.preventDefault();
+    $('input#recipients').val('sender').trigger('change');
   } );
 
-  $('a.reply-to-all').click( function() {
-    $('form#new-message #recipients option').prop('selected', false);
-    $('form#new-message #recipients option[value="'+$('.sender').data('member-id')+'"]').prop('selected', true);
-    $('.recipients .recipient').each( function() {
-      $('form#new-message #recipients option[value="'+$(this).data('member-id')+'"]').prop('selected', true);
-    } );
-    $('form#new-message #recipients option[value="'+$('form#new-message').data('member-id')+'"]').prop('selected', false);
-    $('select#recipients').trigger("liszt:updated");
-    return false;
+  $('a.reply-to-all').click( function (event) {
+    event.preventDefault();
+    $('input#recipients').val('all').trigger('change');
   } );
 
   $('input#recipients').select2({
