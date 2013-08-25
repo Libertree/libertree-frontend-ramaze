e0f564d5c4ed0d1d94442fc1088e8fe1f3df24aa
rekado <rekado@elephly.net>
2013-08-25 08:59:06 +0800

    new contact list form: replace chosen with select2

--- public/js/controller/contact-lists.js
+++ public/js/controller/contact-lists.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
   $('select#contact-list-members').chosen();
+  $('input#contact-list-members').select2(Libertree.UI.selectDefaults);
   $(document).on('click', '.contact-lists .delete', Libertree.UI.confirmAction);
 } );
