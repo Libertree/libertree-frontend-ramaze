5a4025e261a4b147d1f526fe72e1efc1c002db56
Pistos <github.pistos@purepistos.net>
2014-11-04 21:35:56 -0500

    Convert notification click handlers from jQuery to Vue.js .

--- public/js/notifications.js
+++ public/js/notifications.js
@@ -3,9 +3,6 @@ $(document).ready( function() {
     e.stopPropagation();
   } );
 
-  $(document).on('click', '.notification.unseen', Libertree.Notifications.setState('seen') );
-  $(document).on('click', '.notification.seen', Libertree.Notifications.setState('unseen') );
-
   $(document).on('click', '#mark-all-notifications-seen', function(event) {
     event.preventDefault();
     $.get('/notifications/seen/all', function () {
