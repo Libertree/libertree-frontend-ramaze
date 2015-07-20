2544d3e24fce7848521833f47250181d19243951
Pistos <git dawt pistos aet purepistos dawt net>
2015-07-20 19:49:33 -0400

    Invitation code requirement is now optional (config).

--- controller/invitations.rb
+++ controller/invitations.rb
@@ -8,6 +8,8 @@ module Controller
     def create
       if ! $conf['invitations']
         flash[:error] = _('Invitations have been disabled.')
+      elsif ! $conf['invitation_needed']
+        flash[:error] = _('Invitations are not needed for signup.')
       elsif account.new_invitation.nil?
         flash[:error] = _('Failed to create invitation.  You may only have up to 5 unaccepted invitations at once.')
       end
@@ -16,6 +18,11 @@ module Controller
     end
 
     def index
+      if ! $conf['invitation_needed']
+        flash[:error] = _('Invitations are not needed for signup.')
+        redirect_referrer
+      end
+
       @view = "accounts edit"
       @host = request.host_with_port
       @invitations = account.invitations_not_accepted
